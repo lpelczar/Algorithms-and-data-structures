0dd914b65c800826253440d19aa8cb0587d71da8
lpelczar <lukepelczar@gmail.com>
2018-04-27 16:18:21 +0200

    Implement inserting method in doubly linked list

--- Linked-list/src/main/java/com/codecool/doublylinkedlist/DoublyLinkedList.java
+++ Linked-list/src/main/java/com/codecool/doublylinkedlist/DoublyLinkedList.java
@@ -50,6 +50,110 @@ public class DoublyLinkedList<T> {
         size++;
     }
 
+    public void insert(int index, T data) {
+        if (index < 0 || index > size) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+
+        if (index == 0) {
+            addAsFirst(data);
+        }
+
+        int counter = 0;
+        Node<T> current = head;
+        while (current != null) {
+            if (counter == index - 1) {
+                Node<T> insertedNode = new Node<>(data);
+                insertedNode.setNext(current.getNext());
+                if (current.getNext() != null) {
+                    current.getNext().setPrevious(insertedNode);
+                }
+                current.setNext(insertedNode);
+                insertedNode.setPrevious(current);
+                if (index == size) {
+                    tail = insertedNode;
+                }
+                size++;
+            }
+            counter++;
+            current = current.getNext();
+        }
+    }
+
+    public boolean delete(T data) {
+        if (head == null) {
+            return false;
+        }
+
+        boolean isDeleted = false;
+        if (head.getData().equals(data)) {
+            this.head = head.getNext();
+            size--;
+            isDeleted = true;
+        } else {
+            Node<T> current = head;
+            while (current.getNext() != null) {
+                if (current.getNext().getData().equals(data)) {
+                    Node<T> removedNode = current.getNext();
+                    if (removedNode.getNext() != null) {
+                        current.setNext(removedNode.getNext());
+                    } else {
+                        current.setNext(null);
+                        tail = current;
+                    }
+                    size--;
+                    isDeleted = true;
+                    break;
+                }
+                current = current.getNext();
+            }
+        }
+        return isDeleted;
+    }
+
+    public T getByIndex(int index) {
+        if (index < 0 || index > size) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+
+        if (head == null) {
+            throw new ArrayIndexOutOfBoundsException("Empty list");
+        }
+        T data = null;
+        if (index == 0) {
+            data = head.getData();
+        }
+
+        int counter = 0;
+        Node<T> current = head;
+        while (current != null) {
+            if (counter == index) {
+                data = current.getData();
+            }
+            counter++;
+            current = current.getNext();
+        }
+        return data;
+    }
+
+    public void clear() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public boolean contains(T data) {
+        boolean isFound = false;
+        Node<T> current = head;
+        while (current != null) {
+            if (current.getData().equals(data)) {
+                isFound = true;
+            }
+            current = current.getNext();
+        }
+        return isFound;
+    }
+
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
--- Linked-list/src/test/java/com/codecool/doublylinkedlist/DoublyLinkedListTest.java
+++ Linked-list/src/test/java/com/codecool/doublylinkedlist/DoublyLinkedListTest.java
@@ -1,8 +1,10 @@
 package com.codecool.doublylinkedlist;
 
+import com.codecool.singlylinkedlist.SinglyLinkedList;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class DoublyLinkedListTest {
 
@@ -42,6 +44,61 @@ class DoublyLinkedListTest {
         DoublyLinkedList<Integer> linkedList = createList(12);
         linkedList.add(800);
         assertEquals(13, linkedList.size());
+    }
+
+    @Test
+    void whenInsertWithInvalidIndex1ThenThrowArrayBoundsException() {
+        DoublyLinkedList<Integer> linkedList = new DoublyLinkedList<>();
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> linkedList.insert(-1, 10));
+    }
+
+    @Test
+    void whenInsertWithInvalidIndex2ThenThrowArrayBoundsException() {
+        DoublyLinkedList<Integer> linkedList = new DoublyLinkedList<>();
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> linkedList.insert(20, 10));
+    }
+
+    @Test
+    void whenInsertItemThenItemIsProperlyInserted() {
+        DoublyLinkedList<Integer> linkedList = createList(11);
+        linkedList.insert(2, 890);
+        String result = " 0 1 890 2 3 4 5 6 7 8 9 10";
+        assertEquals(result, linkedList.toString());
+    }
+
+    @Test
+    void whenInsertAsFirstModifyHead() {
+        DoublyLinkedList<Integer> linkedList = createList(11);
+        linkedList.insert(0, 890);
+        String result = " 890 0 1 2 3 4 5 6 7 8 9 10";
+        assertEquals(result, linkedList.toString());
+        assertEquals(new Integer(890), linkedList.getFirst());
+    }
+
+    @Test
+    void whenInsertAsLastThenModifyTail() {
+        DoublyLinkedList<Integer> linkedList = createList(11);
+        linkedList.insert(11, 890);
+        String result = " 0 1 2 3 4 5 6 7 8 9 10 890";
+        assertEquals(result, linkedList.toString());
+        assertEquals(new Integer(890), linkedList.getLast());
+    }
+
+    @Test
+    void whenInsertAndListIsEmptyThenAddFirst() {
+        DoublyLinkedList<Integer> linkedList = new DoublyLinkedList<>();
+        linkedList.insert(0, 890);
+        String result = " 890";
+        assertEquals(result, linkedList.toString());
+    }
+
+    @Test
+    void whenInsertTwoTimesThenValuesAreInsertedCorrectly() {
+        DoublyLinkedList<Integer> linkedList = createList(11);
+        linkedList.insert(11, 890);
+        linkedList.insert(1, 890);
+        String result = " 0 890 1 2 3 4 5 6 7 8 9 10 890";
+        assertEquals(result, linkedList.toString());
     }
 
     private DoublyLinkedList<Integer> createList(int numOfElements) {
