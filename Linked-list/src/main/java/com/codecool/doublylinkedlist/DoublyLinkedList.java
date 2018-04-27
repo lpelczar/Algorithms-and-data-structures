00961eeccebad4f1e35592fa5d3cc84c8ebe20e2
lpelczar <lukepelczar@gmail.com>
2018-04-27 16:06:57 +0200

    Implement methods in doubly linked list

--- Linked-list/src/main/java/com/codecool/doublylinkedlist/DoublyLinkedList.java
+++ Linked-list/src/main/java/com/codecool/doublylinkedlist/DoublyLinkedList.java
@@ -1,4 +1,64 @@
 package com.codecool.doublylinkedlist;
 
-public class DoublyLinkedList {
+
+public class DoublyLinkedList<T> {
+
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
+    public DoublyLinkedList() {}
+
+    public DoublyLinkedList(T data) {
+        head = new Node<>(data);
+    }
+
+    public T getFirst() {
+        if (head == null) {
+            throw new ArrayIndexOutOfBoundsException("Empty list");
+        }
+        return head.getData();
+    }
+
+    public T getLast() {
+        if (tail == null) {
+            throw new ArrayIndexOutOfBoundsException("Empty list");
+        }
+        return tail.getData();
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void add(T data) {
+        if (head == null) {
+            head = new Node<>(data);
+        } else {
+            tail = head.append(data);
+        }
+        size++;
+    }
+
+    public void addAsFirst(T data) {
+        Node<T> newHead = new Node<>(data);
+        if (head != null) {
+            newHead.setNext(head);
+            head.setPrevious(newHead);
+        }
+        head = newHead;
+        size++;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        Node<T> current = head;
+        while (current != null) {
+            stringBuilder.append(" ");
+            stringBuilder.append(current.getData());
+            current = current.getNext();
+        }
+        return stringBuilder.toString();
+    }
 }
--- Linked-list/src/test/java/com/codecool/doublylinkedlist/DoublyLinkedListTest.java
+++ Linked-list/src/test/java/com/codecool/doublylinkedlist/DoublyLinkedListTest.java
@@ -1,4 +1,55 @@
 package com.codecool.doublylinkedlist;
 
-public class DoublyLinkedListTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DoublyLinkedListTest {
+
+    @Test
+    void whenInstantiatedThenHeadIsProperlyDefined() {
+        Integer firstElement = 0;
+        DoublyLinkedList<Integer> list = new DoublyLinkedList<>(firstElement);
+        assertEquals(firstElement, list.getFirst());
+    }
+
+    @Test
+    void whenAddMultipleTimesThenItemsAreProperlyAdded() {
+        DoublyLinkedList<Integer> linkedList = createList(11);
+        linkedList.add(33);
+        linkedList.add(45);
+        String result = " 0 1 2 3 4 5 6 7 8 9 10 33 45";
+        assertEquals(result, linkedList.toString());
+    }
+
+    @Test
+    void whenGetLastThenReturnLast() {
+        DoublyLinkedList<Integer> linkedList = createList(11);
+        assertEquals(new Integer(10), linkedList.getLast());
+    }
+
+    @Test
+    void whenAddAsFirstThenChangeHead() {
+        DoublyLinkedList<Integer> linkedList = createList(11);
+        linkedList.addAsFirst(800);
+        String result = " 800 0 1 2 3 4 5 6 7 8 9 10";
+        assertEquals(result, linkedList.toString());
+        assertEquals(new Integer(800), linkedList.getFirst());
+    }
+
+    @Test
+    void whenElementsAddedThenReturnAppropriateLength() {
+        DoublyLinkedList<Integer> linkedList = createList(12);
+        linkedList.add(800);
+        assertEquals(13, linkedList.size());
+    }
+
+    private DoublyLinkedList<Integer> createList(int numOfElements) {
+        DoublyLinkedList<Integer> linkedList = new DoublyLinkedList<>();
+        for (Integer i = 0; i < numOfElements; ++i)
+        {
+            linkedList.add(i);
+        }
+        return linkedList;
+    }
 }
