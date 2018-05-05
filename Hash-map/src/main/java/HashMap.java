0bd0d65061d09a53b7838d0f6aacd9bbe127d1df
lpelczar <lukepelczar@gmail.com>
2018-05-05 17:52:52 +0200

    Implement adding method and to string method

--- Hash-map/src/main/java/HashMap.java
+++ Hash-map/src/main/java/HashMap.java
@@ -1,3 +1,35 @@
-public class HashMap {
+import java.util.LinkedList;
 
+public class HashMap<K, V> {
+
+    private int bucketSize = 16;
+
+    @SuppressWarnings("unchecked")
+    private LinkedList<Entry<K, V>>[] elements = new LinkedList[bucketSize];
+
+    public void add(K key, V value) {
+        int position = getHashPosition(key);
+        LinkedList<Entry<K, V>> list = elements[position];
+
+        for (Entry<K, V> entry : list) {
+            if (entry.getKey() == key) throw new IllegalArgumentException("Key already exists!");
+        }
+        list.add(new Entry<>(key,value));
+        // resizeIfNecessary();
+    }
+
+    private int getHashPosition(K key) {
+        return key.hashCode() % bucketSize;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (LinkedList<Entry<K, V>> elem : elements) {
+            for (Entry<K, V> entry : elem) {
+                stringBuilder.append(" ").append(entry.getKey()).append(":").append(entry.getValue());
+            }
+        }
+        return stringBuilder.toString();
+    }
 }
