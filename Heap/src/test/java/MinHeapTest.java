71e8557b66ccf01a125dc68b61330deb1d3c2bc6
lpelczar <lukepelczar@gmail.com>
2018-05-05 15:36:22 +0200

    Add test checking adding method

--- Heap/src/test/java/MinHeapTest.java
+++ Heap/src/test/java/MinHeapTest.java
@@ -1,4 +1,14 @@
-public class MinHeapTest {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+class MinHeapTest {
+
+    @Test
+    void whenAddThenElementIsAdded() {
+        MinHeap minHeap = new MinHeap();
+        minHeap.add(1);
+        String expected = " 1";
+        assertEquals(expected, minHeap.toString());
+    }
 }
